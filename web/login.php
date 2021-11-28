31acbe9345f2c8a41076a5d794509a929418e740
Semiha Karaibrahimova <semiha_19@abv.bg>
2021-11-28 19:08:09 +0200

    Modified login and register page layout

--- web/login.php
+++ web/login.php
@@ -1,6 +1,10 @@
 <?php
-// web/login.php
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
 ?>
+
 <!doctype html>
 <html lang="en">
     <head>
@@ -10,33 +14,41 @@
 
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+        <link href="css/form-styles.css" type="text/css" rel="stylesheet" media="all">
         <title>Login</title>
+      
+         <style>
+            
+        </style>
+        
     </head>
     <body>
-        <div class="col-md-6 offset-md-3">
-            <form method="post" action="post">
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email address:</label>
-                    <input type="email" name="email" id="email" class="form-control" placeholder="Email">
-                </div>
-                <div class="mb-3">
-                    <label for="pass" class="form-label">Password:</label>
-                    <input type="password" name="pass" id="pass" class="form-control">
-                </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" name="remember" id="remember">
-                    <label for="remember" class="form-check-label">Remember me</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Login</button>
-            </form>
-        </div>
-        <div class="row">
-            <div class="col-md-6 offset-md-3 mt-3">
-                <a href="<?= 'index.php?page=user&action=register'; ?>">Register</a> |
-                <a href="#">Forgot password?</a> |
-                <a href="<?= 'index.php?page=' . DEFAULT_PAGE . '&action=' . DEFAULT_ACTION; ?>">Home page</a>
+        <div class="login-page">
+            <div class="form">
+                <form class="login-form" method="post" action="post">
+
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address:</label>
+                        <input type="email" name="email" id="email" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="pass" class="form-label">Password:</label>
+                        <input type="password" name="pass" id="pass" class="form-control"placeholder="Password">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" name="remember" id="remember">
+                        <label for="remember" class="form-check-label">Remember me</label>
+                    </div>
+                </form>
+                <button>login</button>
+                <p class="message">Not registered? <a href="#">Create an account</a></p>
+                </form>
             </div>
         </div>
     </body>
+    <script>
+        $('.message a').click(function () {
+            $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+        });
+    </script>
 </html>
